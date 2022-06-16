5e1a4036e8248107f533eabfbdd0b5cf0f43d728
RalfsRup <ralfsruperts@gmail.com>
2022-06-16 13:58:02 +0300

    3rd

--- client/src/components/cart/CartCard.js
+++ client/src/components/cart/CartCard.js
@@ -5,9 +5,22 @@ const CartCard = () => {
 
     const [prodquantity, setProdQuantity] = useState(1);
 
+    const increasequantity = () => {
+        setProdQuantity(prodquantity + 1)
+    }
+    const decreasequantity = () => {
+        if (prodquantity >= 1){
+            setProdQuantity(prodquantity -1)
+        }
+    }
+
 
     return(
-        <div>CartCard</div>
+        <div className="cart-prod-container">
+            <div className="cart-prod-imgtitle">
+                <div className="prod-image"><img src={itemdata.itemdata.products.attelsurl}/></div>
+            </div>
+        </div>
     )
 }
 export default CartCard;
